d95b11f9e36d803178478ff2a0427b6307c59e73
Sunny Hasija <hasija.4@osu.edu>
2018-06-22 09:22:01 -0400

    Python Scriots to Download Tweets for Research
    The keywordscraper.py file allows you to download tweets containing a particular keyword from the public twitter stream.
    
    The profilescraper.py file allows you to download tweets from any public twitter profile. It paginates through the profile to download all tweets (actually 3200, most the twitter API allows) from that profile.

---
+++ keywordscraper.py
@@ -0,0 +1,39 @@
+import tweepy #Package that allows you to use the Twitter API
+import csv  #Package to read and write CSV
+import pandas as pd #Data frame package from Python
+
+#You need to get your Twitter API credentials to interact with twitter from https://developer.twitter.com/
+
+####input your credentials here
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+
+#authorize With Twitter
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+#lets get some tweets
+# Open/Create a file to append data
+csvFile = open('filename.csv','a',encoding='utf-8')
+#Use csv Writer
+csvWriter = csv.writer(csvFile)
+csvWriter.writerow(["screenname","id","created_at","text"])
+outtweets=[]
+
+#get the tweets for the particular keyword from the search API - we are setting the optional limit to 100 tweets at a time
+for tweet in tweepy.Cursor(api.search,q="keyword/hashtag",count=100,lang="en",
+                           since="2018-06-11").items():
+    #display the tweets
+    print (tweet.user.screen_name,tweet.id, tweet.created_at, tweet.text)
+    #convert the tweets into a 2D array
+    outtweets=[tweet.user.screen_name,tweet.id, tweet.created_at, tweet.text]
+    #write them to the CSV
+    csvWriter.writerow(outtweets)
+
+#close the file
+csvFile.close()
---
+++ profilescraper.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import tweepy #https://github.com/tweepy/tweepy
+import csv #to write tweets to CSV file
+
+#Twitter API credentials
+#you can get this from developer.twitter.com and registering your application
+
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+
+def get_all_tweets(screen_name):
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []
+
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        print ("getting tweets before %s" % (oldest))
+
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+
+        print ("...%s tweets downloaded so far" % (len(alltweets)))
+
+    #transform the tweepy tweets into a 2D array that will populate the csv
+    #encode the text in utf-8 for ease of processing. You can also do latin-1
+
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8"),tweet.retweet_count] for tweet in alltweets]
+
+    #write the csv
+    with open('%s_tweets.csv' % screen_name, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["id","created_at","text", "retweet_count"])
+        writer.writerows(outtweets)
+
+    pass
+
+
+if __name__ == '__main__':
+    #pass in the username(s) of the account you want to download
+    foo = ['username1','ohiostate'] #pass any number that you want separated by a comma
+    for handle in foo:
+        get_all_tweets( handle)
+        print(" All tweets for %s downloaded" % handle)
